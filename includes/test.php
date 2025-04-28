a359e926b61efa8c8d500265545cea542d721ea3
4ttth <78966341+4ttth@users.noreply.github.com>
2025-04-28 11:45:55 +0800

    Debugging

--- includes/test.php
+++ includes/test.php
@@ -11,6 +11,8 @@ $ocrResult2 = json_decode($output, true);
 echo "jsonResult" . $ocrResult1 . "<br><br>";
 echo "shellResult" . $ocrResult2 . "<br><br>";
 echo "Raw Shell Outpu: " . $output . "<br><br>";
+echo shell_exec("/usr/bin/python3 --version 2>&1") . "<br><br>";
+echo shell_exec("/usr/bin/python3 -m pip list 2>&1") . "<br><br>";
 ?>
 
 </body>
