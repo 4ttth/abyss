365837678b647a2d17cdc750d4e9fe588bf15a3d
4ttth <78966341+4ttth@users.noreply.github.com>
2025-04-28 16:10:19 +0800

    Debugging

--- includes/test.php
+++ includes/test.php
@@ -9,8 +9,10 @@ $jsonResult = '{"resultStatus": "Defeat", "battleID": "201965272562210724"}';
 $ocrResult1 = json_decode($jsonResult, true);
 $ocrResult2 = json_decode($output, true);
 echo "jsonResult" . $ocrResult1 . "<br><br>";
+echo "jsonResult" . $ocrResult1[0] . "<br><br>";
+echo "jsonResult" . $ocrResult1[1] . "<br><br>";
 echo "shellResult" . $ocrResult2 . "<br><br>";
-echo "Raw Shell Outpu: " . $output . "<br><br>";
+echo "Raw Shell Output: " . $output . "<br><br>";
 echo shell_exec("/usr/bin/python3 --version 2>&1") . "<br><br>";
 echo shell_exec("/usr/bin/python3 -m pip list 2>&1") . "<br><br>";
 if (strpos(strtolower($resultStatus), 'victory') !== false) {
