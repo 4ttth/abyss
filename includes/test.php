0f3ca728661e40a3bf2e5afcc7f2a240113f924c
4ttth <78966341+4ttth@users.noreply.github.com>
2025-04-28 11:40:53 +0800

    Debugging

--- includes/test.php
+++ includes/test.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$command = "python3 /var/www/html/abyss/Python/ML-OCR/battleResults.py /var/www/html/abyss/uploads/match_proofs/proof_680ef2998a6260.33876361.PNG 2>&1";
+$command = "/usr/bin/python3 /var/www/html/abyss/Python/ML-OCR/battleResults.py /var/www/html/abyss/uploads/match_proofs/proof_680ef2998a6260.33876361.PNG 2>&1";
 $output = shell_exec($command);
 $jsonResult = '{"resultStatus": "Defeat", "battleID": "201965272562210724"}';
 $ocrResult1 = json_decode($jsonResult, true);
