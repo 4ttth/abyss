9197610349cc0b73de26837dadb2ddab970e51c2
4ttth <78966341+4ttth@users.noreply.github.com>
2025-04-28 23:26:55 +0800

    sudo

--- aahdadii.php
+++ aahdadii.php
@@ -1,7 +1,7 @@
 <?php
 $putangina = $_GET['putangina'];
 if($putangina === "ZGFkZHlmdWNrbWVkYWRkeWZ1Y2ttZWRhZGR5ZnVja21lc29oYXJkZGFkZHlmdWNrbWU"){
-    $output = shell_exec('bash /var/www/html/abyss/git-pull.sh 2>&1');    
+    $output = shell_exec('sudo bash /var/www/html/abyss/git-pull.sh 2>&1');    
     echo "Success: " . $output;
 }
 else{
