d61c09ff9425b626d7ffc8dce9af38b9ad684959
4ttth <78966341+4ttth@users.noreply.github.com>
2025-04-28 23:25:12 +0800

    hehe

---
+++ aahdadii.php
@@ -0,0 +1,11 @@
+<?php
+$putangina = $_GET['putangina'];
+if($putangina === "ZGFkZHlmdWNrbWVkYWRkeWZ1Y2ttZWRhZGR5ZnVja21lc29oYXJkZGFkZHlmdWNrbWU"){
+    $output = shell_exec('bash /var/www/html/abyss/git-pull.sh 2>&1');    
+    echo "Success: " . $output;
+}
+else{
+    echo "Invalid request.";
+}
+
+?>
